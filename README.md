1505401617ca4b54049bc108ad741720d201ced5
clarissagtz <cgutierrezgodoy@ucsd.edu>
2022-11-20 11:52:05 -0800

    finalize lab8

--- README.md
+++ README.md
@@ -3,11 +3,21 @@ Clarissa Gutierrez
 
 Sashwat Nayak
  
+QUESTIONS: 
 
-1.  Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+1.  **Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.**
  
- Ans: Within a Github action that runs whenever code is pushed, because running tests manually takes a lot of time and could also contribute to human error. Furthermore, running all tests after development is complete requires multiple testing procedures to occur at one time, meaning we cannot see how specific features work.
+Within a Github action that runs whenever code is pushed, because running tests manually takes a lot of time and could also contribute to human error. Furthermore, running all tests after development is complete requires multiple testing procedures to occur at one time, meaning we cannot see how specific features work.
 
- 2) Would you use an end to end test to check if a function is returning the correct output? (yes/no) 
+2. **Would you use an end to end test to check if a function is returning the correct output? (yes/no)**
  
- Ans. No.
+No.
+
+3. **Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.**
+
+The messaging feature of an app would be more appropriately tested by E2E testing than a unit test since it tests the functionality between a feature that reacts to the components of the app.
+
+
+4. **Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.**
+
+ The max message length feature of an app would be appropriately tested by a unit test since it reacts with a part of the code that ensures the creation of the message.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,96 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+// four tests for isPhoneNumber
+
+test('Phone # "310-703-4310" is valid', () => {
+    expect(functions.isPhoneNumber('310-703-4310')).toBe(true);
+  });
+  
+
+test('Phone # "(+1) 916-038-2038"', () => {
+   expect(functions.isPhoneNumber('(+1) 916-038-2038')).toBe(true);
+ });
+
+test('Phone # "3409" is invalid', () => {
+    expect(functions.isPhoneNumber('3409')).toBe(false);
+});
+
+test('Phone # "a82y" is invalid', () => {
+    expect(functions.isPhoneNumber('a82y')).toBe(false);
+});
+
+// four tests for isEmail
+
+test('Email "adamsandler34@gmail.com" is valid', () => {
+    expect(functions.isEmail('adamsandler34@gmail.com')).toBe(true);
+});
+
+test('Email "metsfan@hotmail.com" is valid', () => {
+    expect(functions.isEmail('metsfan@hotmail.com')).toBe(true);
+});
+
+test('Email "123" is invalid', () => {
+    expect(functions.isEmail('123')).toBe(false);
+});
+
+test('Email "$$$$$$" is invalid', () => {
+    expect(functions.isEmail('$$$$$$')).toBe(false);
+});
+
+// four tests for isStrongPassword
+
+
+test('Password is "f***"', () => {
+    expect(functions.isStrongPassword()).toBe(true);
+});
+
+  test('Password is "u019sj_"', () => {
+    expect(functions.isStrongPassword()).toBe(true);
+  });
+
+  test('Password "a" is not strong', () => {
+    expect(functions.isStrongPassword('a')).toBe(false);
+  });
+
+  test('Password "this is weak" is not strong', () => {
+    expect(functions.isStrongPassword('this is weak')).toBe(false);
+  });
+
+// four tests for isDate
+
+test('Date "11/20/2022" is valid', () => {
+    expect(functions.isDate('11/20/2022')).toBe(true);
+  });
+
+  test('Date "4/23/1977" is valid', () => {
+    expect(functions.isDate('4/23/1977')).toBe(true);
+  });
+
+  test('Date "c:" is invalid', () => {
+    expect(functions.isDate('c:')).toBe(false);
+  });
+
+  test('Date "782/840/29334" is invalid', () => {
+    expect(functions.isDate('782/840/29334')).toBe(false);
+  });
+  
+
+// four tests for isHexColor*/
+
+test('Hex Code "#093" is valid', () => {
+    expect(functions.isHexColor('#093')).toBe(true);
+  });
+
+  test('Hex Code "#fff093" is valid', () => {
+    expect(functions.isHexColor('#fff093')).toBe(true);
+  });
+  
+  test('Hex Code "goodkidmaadcity" is invalid', () => {
+    expect(functions.isHexColor('goodkidmaadcity')).toBe(false);
+  });
+
+  test('Hex Code "-_-" is invalid', () => {
+    expect(functions.isHexColor('-_-')).toBe(false);
+  });
